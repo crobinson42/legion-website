a486f8af70f2610d6d746498e015387e54888017
cory robinson <coryrobinson42@gmail.com>
2016-06-29 20:52:44 -0700

    change search dropdown to therms link

--- html_partials/header/header_nav_login.php
+++ html_partials/header/header_nav_login.php
@@ -3,16 +3,8 @@
 <section>
   <div class="container">
     <div class="sform">
-      <a class="search-form_toggle" href="#"></a>
+      <a class="search-form_toggle" href="http://www.therms.io/login"></a>
 
-      <form class="search-form" action="search.php" method="GET" accept-charset="utf-8">
-        <label class="search-form_label">
-          <input class="search-form_input" type="text" name="s" autocomplete="off"
-                 placeholder="Username"/>
-          <span class="search-form_liveout"></span>
-        </label>
-        <button class="search-form_submit fa-search" type="submit"></button>
-      </form>
     </div>
   </div>
 </section>
