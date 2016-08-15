f932db2b556c6be4a5c707a108cd8047d9f9d5f6
cory robinson <coryrobinson42@gmail.com>
2016-08-15 13:23:41 -0700

    add images and rearrange contact us page

--- contact-us.php
+++ contact-us.php
@@ -24,7 +24,7 @@
   <!--JS-->
   <script src="js/jquery.js"></script>
   <script src="js/jquery-migrate-1.2.1.min.js"></script>
-   
+
 
 
   <!--[if lt IE 9]>
@@ -65,7 +65,81 @@
     include dirname(__FILE__) .'/html_partials/sections/request_quote.php';
     ?>
 
-    <section class="well well7">
+    <section style="margin:50px 15px;">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              Business Operations
+            </div>
+            <div class="panel-body">
+              <ul>
+                <li>Service Quotes & Proposals</li>
+                <li>Accounts Receivable/Payable</li>
+                <li>Payroll</li>
+                <li>Insurance & Licensing</li>
+              </ul>
+              <p>
+                <h6>Ronald "Ron" Robinson</h6>
+                Chief Executive Officer
+                <br>
+                <i class="fa fa-phone" aria-hidden="true"></i> <a href="callto:9168771580">916-877-1580</a>
+                <br>
+                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:ronald.robinson@legionsecurity.us">ronald.robinson@legionsecurity.us</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              Security Operations
+            </div>
+            <div class="panel-body">
+              <ul>
+                <li>Client Services, Requests, & Changes</li>
+                <li>Scheduling</li>
+                <li>Uniformed Staff Management</li>
+                <li>Patrol Vehicles & Fleet</li>
+              </ul>
+              <p>
+                <h6>William "Bill" Preston</h6>
+                Chief Operations Officer
+                <br>
+                <i class="fa fa-phone" aria-hidden="true"></i> <a href="callto:9162050026">916-205-0026</a>
+                <br>
+                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:william.preston@legionsecurity.us">william.preston@legionsecurity.us</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              24 Hour Dispatch
+            </div>
+            <div class="panel-body">
+              <ul>
+                <li>Camera Activity Report</li>
+                <li>Client/Resident/Tenant Immediate Security Issues</li>
+                <li>Suspicious Persons, Vehicle, or Activity</li>
+                <li>Noise, Disturbance, or Fighting</li>
+                <li>Community Parking Violation</li>
+              </ul>
+              <p>
+                <h6>Sacramento Dispatch</h6>
+                <i class="fa fa-phone" aria-hidden="true"></i> <a href="callto:9165502746">916-550-2746</a>
+                <br>
+                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:dispatch@legionsecurity.us">dispatch@legionsecurity.us</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="well well7" style="background-image:url('/images/legion/pic_ron_bill_1000.jpg')">
       <div class="container">
         <div class="row">
           <div class="col-md-4 col-xs-12">
@@ -155,78 +229,36 @@
       </div>
     </section>
 
-    <section style="margin:0 15px;">
+
+    <section style="margin:50px 15px;">
       <div class="row">
-        <div class="col-md-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">
-              Business Operations
-            </div>
-            <div class="panel-body">
-              <ul>
-                <li>Service Quotes & Proposals</li>
-                <li>Accounts Receivable/Payable</li>
-                <li>Payroll</li>
-                <li>Insurance & Licensing</li>
-              </ul>
-              <p>
-                <h6>Ronald "Ron" Robinson</h6>
-                Chief Executive Officer
-                <br>
-                <i class="fa fa-phone" aria-hidden="true"></i> <a href="callto:9168771580">916-877-1580</a>
-                <br>
-                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:ronald.robinson@legionsecurity.us">ronald.robinson@legionsecurity.us</a>
-              </p>
-            </div>
-          </div>
+
+        <div class="col-md-10 col-md-offset-1 text-center" style="margin-bottom:50px;">
+          <h3 class="text-muted">Connect With Us!</h3>
+          <hr/>
         </div>
-        <div class="col-md-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">
-              Security Operations
-            </div>
-            <div class="panel-body">
-              <ul>
-                <li>Client Services, Requests, & Changes</li>
-                <li>Scheduling</li>
-                <li>Uniformed Staff Management</li>
-                <li>Patrol Vehicles & Fleet</li>
-              </ul>
-              <p>
-                <h6>William "Bill" Preston</h6>
-                Chief Operations Officer
-                <br>
-                <i class="fa fa-phone" aria-hidden="true"></i> <a href="callto:9162050026">916-205-0026</a>
-                <br>
-                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:william.preston@legionsecurity.us">william.preston@legionsecurity.us</a>
-              </p>
-            </div>
-          </div>
+
+        <div class="col-md-4 text-center">
+          <a href="https://plus.google.com/u/1/107926698583162354167">
+            <img src="/images/google-plus.png" alt="Join Legion Security on Google Plus!" width="100" />
+          </a>
         </div>
-        <div class="col-md-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">
-              24 Hour Dispatch
-            </div>
-            <div class="panel-body">
-              <ul>
-                <li>Camera Activity Report</li>
-                <li>Client/Resident/Tenant Immediate Security Issues</li>
-                <li>Suspicious Persons, Vehicle, or Activity</li>
-                <li>Noise, Disturbance, or Fighting</li>
-                <li>Community Parking Violation</li>
-              </ul>
-              <p>
-                <h6>Sacramento Dispatch</h6>
-                <i class="fa fa-phone" aria-hidden="true"></i> <a href="callto:9165502746">916-550-2746</a>
-                <br>
-                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:dispatch@legionsecurity.us">dispatch@legionsecurity.us</a>
-              </p>
-            </div>
-          </div>
+
+        <div class="col-md-4 text-center">
+          <a href="https://www.linkedin.com/company/10701382?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A10701382%2Cidx%3A3-2-4%2CtarId%3A1471285808712%2Ctas%3Alegion%20security">
+            <img src="/images/linkedin.png" alt="Join Legion Security on Linkedin.com" width="100" />
+          </a>
         </div>
+
+        <div class="col-md-4 text-center">
+          <a href="http://www.yelp.com/biz/legion-security-sacramento-2">
+            <img src="/images/yelp.png" alt="Join Legion Security on yelp.com" width="100" />
+          </a>
+        </div>
+
       </div>
     </section>
+
 
   </main>
 
